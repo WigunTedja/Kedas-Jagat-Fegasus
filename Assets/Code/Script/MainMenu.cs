a0dfb83cc744c36a2e16d66f74be4a307cb55775
Wiguna Tedja <gustedja123@gmail.com>
2026-09-24 21:19:47 +0800

    fixed button bug

--- Assets/Code/Script/MainMenu.cs
+++ Assets/Code/Script/MainMenu.cs
@@ -23,14 +23,15 @@ public class MainMenu : MonoBehaviour
     public void OnPlayButtonPressed()
     {
         // Check if the player has seen the intro. We use 0 as the default if the key doesn't exist.
-        if (PlayerPrefs.GetInt(IntroFlag, 0) == 0)
-        {
-            SceneManager.LoadScene("Game_Intro");
-        }
-        else
-        {
-            SceneManager.LoadScene("Level_Panel");
-        }
+        //if (PlayerPrefs.GetInt(IntroFlag, 0) == 0)
+        //{
+        //    SceneManager.LoadScene("Game_Intro");
+        //}
+        //else
+        //{
+        //    SceneManager.LoadScene("Level_Panel");
+        //}
+        SceneManager.LoadScene("Level_Panel");
     }
 
     public void SettingButton()
